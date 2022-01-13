b793ac8d86d94718dc5b9ae920ec852278464050
Brian Kim <brian.kyounghoon.kim@gmail.com>
2022-01-13 14:59:31 -0500

    ignore dist file, edit source file

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -1,22 +1,5 @@
 export default [
   {
-    name: "environmentMapTexture",
-    type: "cubeTexture",
-    path: [
-      "/textures/environmentMap/px.jpg",
-      "/textures/environmentMap/nx.jpg",
-      "/textures/environmentMap/py.jpg",
-      "/textures/environmentMap/ny.jpg",
-      "/textures/environmentMap/pz.jpg",
-      "/textures/environmentMap/nz.jpg",
-    ],
-  },
-  {
-    name: "foxModel",
-    type: "gltfModel",
-    path: "models/fox/glTF/Fox.gltf",
-  },
-  {
     name: "youniverseModel",
     type: "gltfModel",
     path: "models/youniverse/youniverseStage.gltf",
